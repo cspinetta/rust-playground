f52524e88f7b39189439c167ebb8695cb01f4c85
Cristian Spinetta <cebs91@gmail.com>
2017-06-18 20:20:00 -0300

    wip

--- src/enum_examples.rs
+++ src/enum_examples.rs
@@ -13,10 +13,19 @@ struct IpAddress {
     address: String
 }
 
+// Only with enums:
+enum IpAddrLight {
+    IpV4(u8, u8, u8, u8),
+    IpV6(String)
+}
+
 pub fn test() {
     let four = IpVersion::V4;
     let six = IpVersion::V6;
     let localhost = IpAddress { version: IpVersion::V4, address: String::from("127.0.0.1") };
+
+    let ip4 = IpAddrLight::IpV4(192, 0, 0, 1);
+    let ip6 = IpAddrLight::IpV6(String::from("::1"));
 
     println!("My localhost address is: {:?}", localhost);
 }
---
+++ src/lifetime_examples.rs
@@ -0,0 +1,14 @@
+
+//&i32        // a reference
+//&'a i32     // a reference with an explicit lifetime
+//&'a mut i32 // a mutable reference with an explicit lifetime
+
+fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
+    if x.len() > y.len() {
+        x
+    } else {
+        y
+    }
+}
+
+
